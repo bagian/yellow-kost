28ea7697dd4e237e424f4a23f14db302be526294
Aprilandi <48023542+Aprilandi@users.noreply.github.com>
2025-08-24 20:49:32 +0700

    role++

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function Role() {
+        return $this->belongsTo(Role::class, 'id_role');
+    }
 }
